9424b4a285b95d71ff4c96177a05d0e8b4d417ef
dnakov <daniel.nakov@gmail.com>
2014-07-08 16:20:24 -0400

    Update ngForce-visualForceRemoting.js

--- jsSrc/ngForce-visualForceRemoting.js
+++ jsSrc/ngForce-visualForceRemoting.js
@@ -22,23 +22,23 @@ angular.module('ngForce').provider('vfr', [function () {
       }
       var vfRemote = {};
       /*
-			 * Kevin o'Hara released premote, a nice lib for wrapping
-			 * visualforce remoting calls in a promise interface. this
-			 * function .send() is largely a gentle refactoring of his
-			 * work, found in "premote" here:
-			 *		https://github.com/kevinohara80/premote
-			 * such that it locks into the ng exec loop and utilizes
-			 * the angular $q service, itself based on the Q lib
-			 * Kevin uses.
-			 */
+       * Kevin o'Hara released premote, a nice lib for wrapping
+       * visualforce remoting calls in a promise interface. this
+       * function .send() is largely a gentle refactoring of his
+       * work, found in "premote" here:
+       *    https://github.com/kevinohara80/premote
+       * such that it locks into the ng exec loop and utilizes
+       * the angular $q service, itself based on the Q lib
+       * Kevin uses.
+       */
       /**
-			 * Returns a function that, when called, invokes the js
-			 * remoting method specified in this call.
-			 * @param  {String}   remoteAction class.methodName string representing the Apex className and Method to invoke
-			 * @param  {Object}   options      Ojbect containing at least the timeout and escaping options. Passed to Remoting call
-			 * @param  {Boolean}  nullok       Can this method return null and it be OK?
-			 * @return {Function}              Function engaged with the NG execution loop, making Visualforce remoting calls.
-			 */
+       * Returns a function that, when called, invokes the js
+       * remoting method specified in this call.
+       * @param  {String}   remoteAction class.methodName string representing the Apex className and Method to invoke
+       * @param  {Object}   options      Ojbect containing at least the timeout and escaping options. Passed to Remoting call
+       * @param  {Boolean}  nullok       Can this method return null and it be OK?
+       * @return {Function}              Function engaged with the NG execution loop, making Visualforce remoting calls.
+       */
       vfRemote.send = function (remoteAction, options, nullok) {
         var namespace, controller, method;
         var Manager = Visualforce.remoting.Manager;
@@ -78,13 +78,13 @@ angular.module('ngForce').provider('vfr', [function () {
         };
       };
       /**
-			 * Method returns an Angular promise as the product of a .send() prototyped method call
-			 * @param  {String}   result   Raw JSON string returned by js Remoting call
-			 * @param  {Object}   event    Status object returned from SF detailing errors, if any.
-			 * @param  {Boolean}  nullok   Can the result be null?
-			 * @param  {Deferred} deferred Angular Promise object
-			 * @return {Deferred}          Angular promise with resolution
-			 */
+       * Method returns an Angular promise as the product of a .send() prototyped method call
+       * @param  {String}   result   Raw JSON string returned by js Remoting call
+       * @param  {Object}   event    Status object returned from SF detailing errors, if any.
+       * @param  {Boolean}  nullok   Can the result be null?
+       * @param  {Deferred} deferred Angular Promise object
+       * @return {Deferred}          Angular promise with resolution
+       */
       handleResultWithPromise = function (result, event, nullok, deferred) {
         if (result) {
           result = JSON.parse(result);
@@ -107,18 +107,18 @@ angular.module('ngForce').provider('vfr', [function () {
         }
       };
       /**
-			 * Object contains the two standard fields needed by the .send method: escape and timeout.
-			 * escape: Should the result be escape. default to false.
-			 * timeout: set the timeout for visualforce to respond.
-			 * @type {Object}
-			 */
+       * Object contains the two standard fields needed by the .send method: escape and timeout.
+       * escape: Should the result be escape. default to false.
+       * timeout: set the timeout for visualforce to respond.
+       * @type {Object}
+       */
       var standardOptions = {
           escape: false,
           timeout: 10000
         };
       /*
-			 * This is the setter for standard options, so a config block can overwrite
-			 */
+       * This is the setter for standard options, so a config block can overwrite
+       */
       this.setStandardOptions = function (newOptions) {
         if (newOptions && typeof newOptions !== 'object') {
           throw new Error('standardOptions must be an object');
@@ -126,45 +126,45 @@ angular.module('ngForce').provider('vfr', [function () {
         standardOptions = newOptions;
       };
       // Bulk Create
-      vfRemote.bulkCreate = vfRemote.send('ngForceController3.bulkCreate', standardOptions, false);
+      vfRemote.bulkCreate = vfRemote.send('ngForceController.bulkCreate', standardOptions, false);
       // Bulk Update
-      vfRemote.bulkUpdate = vfRemote.send('ngForceController3.bulkUpdate', standardOptions, false);
+      vfRemote.bulkUpdate = vfRemote.send('ngForceController.bulkUpdate', standardOptions, false);
       // Create
-      vfRemote.create = vfRemote.send('ngForceController3.create', standardOptions, false);
+      vfRemote.create = vfRemote.send('ngForceController.create', standardOptions, false);
       // Clone
-      vfRemote.clone = vfRemote.send('ngForceController3.sObjectKlone', standardOptions, false);
+      vfRemote.clone = vfRemote.send('ngForceController.sObjectKlone', standardOptions, false);
       // Delete
-      vfRemote.del = vfRemote.send('ngForceController3.del', standardOptions, true);
+      vfRemote.del = vfRemote.send('ngForceController.del', standardOptions, true);
       // Describe
-      vfRemote.describe = vfRemote.send('ngForceController3.describe', standardOptions, false);
+      vfRemote.describe = vfRemote.send('ngForceController.describe', standardOptions, false);
       // Describe Field Set
-      vfRemote.describeFieldSet = vfRemote.send('ngForceController3.describeFieldSet', standardOptions, false);
+      vfRemote.describeFieldSet = vfRemote.send('ngForceController.describeFieldSet', standardOptions, false);
       // Describe Picklist Values
-      vfRemote.describePicklistValues = vfRemote.send('ngForceController3.getPicklistValues', standardOptions, false);
+      vfRemote.describePicklistValues = vfRemote.send('ngForceController.getPicklistValues', standardOptions, false);
       // Get Object Type
-      vfRemote.getObjectType = vfRemote.send('ngForceController3.getObjType', standardOptions, false);
+      vfRemote.getObjectType = vfRemote.send('ngForceController.getObjType', standardOptions, false);
       // Get Query Results as select2 data
-      vfRemote.getQueryResultsAsSelect2Data = vfRemote.send('ngForceController3.getQueryResultsAsSelect2Data', standardOptions, false);
+      vfRemote.getQueryResultsAsSelect2Data = vfRemote.send('ngForceController.getQueryResultsAsSelect2Data', standardOptions, false);
       // Query
-      vfRemote.query = vfRemote.send('ngForceController3.query', {
+      vfRemote.query = vfRemote.send('ngForceController.query', {
         escape: false,
         timeout: 30000
       }, false);
       // Query from Fieldset
-      vfRemote.queryFromFieldset = vfRemote.send('ngForceController3.queryFromFieldSet', {
+      vfRemote.queryFromFieldset = vfRemote.send('ngForceController.queryFromFieldSet', {
         escape: false,
         timeout: 30000
       }, false);
       // Retrieve a field list for a given object.
-      vfRemote.retrieve = vfRemote.send('ngForceController3.retrieve', standardOptions, false);
+      vfRemote.retrieve = vfRemote.send('ngForceController.retrieve', standardOptions, false);
       // Search (SOSL)
-      vfRemote.search = vfRemote.send('ngForceController3.search', standardOptions, false);
+      vfRemote.search = vfRemote.send('ngForceController.search', standardOptions, false);
       // Soql from Fieldset
-      vfRemote.soqlFromFieldSet = vfRemote.send('ngForceController3.soqlFromFieldSet', standardOptions, false);
+      vfRemote.soqlFromFieldSet = vfRemote.send('ngForceController.soqlFromFieldSet', standardOptions, false);
       // Update
-      vfRemote.update = vfRemote.send('ngForceController3.updat', standardOptions, true);
+      vfRemote.update = vfRemote.send('ngForceController.updat', standardOptions, true);
       // Upsert
-      vfRemote.upsert = vfRemote.send('ngForceController3.upser', standardOptions, true);
+      vfRemote.upsert = vfRemote.send('ngForceController.upser', standardOptions, true);
       return vfRemote;
     };
   }]);
